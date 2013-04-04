5c13eb228892a68fa2b017f40bca88e8e2530a36
Trilok Arora <Trilok>
2013-04-04 15:13:03 -0400

    Added random fortune

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -11,8 +11,32 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git!");
-            Console.WriteLine("Modified");
+            Console.WriteLine("Todays fortunre = " + GetFortune());
             Console.ReadKey();
+        }
+
+        static string GetFortune()
+        {
+            Random r = new Random();
+            int whichOne = r.Next(56);
+            switch (whichOne % 4)
+            {
+                case 0:
+                    return "this is 0";
+                    
+                case 1:
+                    return "this is 1";
+                    
+                case 2:
+                    return "this is 2";
+                    
+                case 3:
+                    return "this is 3";
+                    
+                default:
+                    return "In default";
+
+            }
         }
     }
 }
